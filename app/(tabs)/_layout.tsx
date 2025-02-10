c0a4abfd896722658f2204f67cbd1f64154f2f27
You Song Hou <ysongweb3@gmail.com>
2025-02-09 23:42:25 -0500

    feat: add tab screen for test

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -47,6 +47,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="count"
+        options={{
+          title: 'Test',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="q.circle" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
